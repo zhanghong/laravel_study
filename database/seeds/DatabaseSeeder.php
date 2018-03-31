7ed855ca696aaae7b66522101f652fb4fa71ae23
zhanghongbean <zhanghongbeam@gmail.com>
2018-03-31 14:27:11 +0000

    Add user statuses

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
       Model::unguard();
       $this->call(UsersTableSeeder::class);
+      $this->call(StatusesTableSeeder::class);
+
       Model::reguard();
     }
 }
