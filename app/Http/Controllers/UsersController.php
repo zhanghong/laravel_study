ed4ce66b780b01579d44bc100431ec7d8cf3689a
zhanghongbean <zhanghongbeam@gmail.com>
2018-03-31 13:46:02 +0000

    Add account activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Mail;
+use Auth;
 class UsersController extends Controller
 {
   public function __construct(){
     $this->middleware('auth', [
-      'except' => ['show', 'create', 'store', 'index']
+      'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
     ]);
 
     $this->middleware('guest', [
@@ -43,9 +45,9 @@ class UsersController extends Controller
       'password' => bcrypt($request->password),
     ]);
 
-    Auth::login($user);
+    $this->sendEmailConfirmationTo($user);
     session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-    return redirect()->route('users.show', [$user]);
+    return redirect('/');
   }
 
   public function edit(User $user){
@@ -79,5 +81,30 @@ class UsersController extends Controller
     $user->delete();
     session()->flash('success', '成功删除用户！');
     return back();
+  }
+
+  public function confirmEmail($token){
+    $user = User::where('activation_token', $token)->firstOrFail();
+
+    $user->activated =  true;
+    $user->activation_token = null;
+    $user->save();
+
+    Auth::login($user);
+    session()->flash('success', '恭喜你，激活成功！');
+    return redirect()->route('users.show', [$user]);
+  }
+
+  protected function sendEmailConfirmationTo($user){
+    $view = 'emails.confirm';
+    $data = compact('user');
+    $from = 'zhanghong_bean@126.com';
+    $name = 'zhanghong';
+    $to = $user->email;
+    $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+
+    Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+      $message->from($from, $name)->to($to)->subject($subject);
+    });
   }
 }
